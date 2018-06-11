a066e4771f39a9556429b78a87ae5f6e0e0091b8
Halil Özel <halilozel1903@gmail.com>
2018-06-11 13:33:35 +0300

    Degiskenler ornekleri eklendi.

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+// var kısaltması variable sözcüğünden gelmektedir. Değiştirilebilir değişkenlerde kullanılır.
+// let engel anlamındadır. Değiştirilemeyen sabit değerler için kullanılır.
+
+import UIKit
+
+var name = "Halil" // name isminde bir değişken tanımı
+
+name // değişken içeriği playground'da gösterildi.
+
+name = "Ibrahim" // name değişkeni güncellendi.
+
+name // güncellenen değişkenin içeriği playground'da gösterildi.
+
+let surname = "Ozel" // surname adında sabit bir değişken tanımı
+
+surname // değişken içeriği playground'da gösterildi.
+
+// surname = "ÖZEL" // Cannot assign to value: 'surname' is a 'let' constant hatasını veriyor.
+// surname içeriğini değiştirmek için ya let olan türü var yapmamız lazım ya da içeriği değiştirilmemelidir.
+
+
