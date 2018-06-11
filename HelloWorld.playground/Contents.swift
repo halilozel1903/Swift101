20cea571c2b643214f8a2ffaee6b7426dd65c82e
Halil Özel <halilozel1903@gmail.com>
2018-06-11 13:36:29 +0300

    Hello world ornegi eklendi.

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+
+import UIKit
+
+var str = "Hello World" // str adında bir degisken tanımlandı.Degiskene "Hello World" yazısı eklendi.
+
+print(str) // print metodu ile mesaj ekranda gosterildi.
+
+
+
