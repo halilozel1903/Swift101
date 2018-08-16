c80835ba345f52eab990c036d7b75be796da1fe3
Halil Özel <halilozel1903@gmail.com>
2018-08-16 20:18:26 +0300

    ConvertDataTypes dersi eklendi.

---
+++ ConvertDataTypes.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+
+import UIKit
+
+// convert string to int
+var strNumber : String = "123"
+var intNumber : Int? = Int(strNumber)
+
+// convert int to string
+var intString : Int = 6789
+var strString : String? = String(intString)
+
+// convert double to int
+var doubleNumber : Double = 12.34
+var integerNumber : Int  = Int(doubleNumber)
+
+// convert int to double
+var intNumber1 : Int = 987
+var doubleNumber1 : Double = Double(intNumber1)
+
+// convert bool to string
+var boolVariable : Bool = true
+var stringVariable : String = String(boolVariable)
+
+
