202845a0b7f4d72a2fed81d04eff0760e69f42f6
Halil Özel <halilozel1903@gmail.com>
2018-08-16 23:37:28 +0300

    hesap makinesi örneği eklendi.

---
+++ Calculator.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/* priorities in math
+ 1 - ()
+ 2 - *, /
+ 3 - +,-
+ 4 - =
+ */
+
+// iki değişken tanımlandı.
+var a = 10
+var b = 5
+
+var sum = a + b // toplama işlemi
+var sub = a - b // çıkarma işlemi
+var mul = a * b // çarpma işlemi
+var div = a / b // bölme işlemi
+
+print(sum) // toplamanın sonucu
+print(sub) // çıkarmanın sonucu
+print(mul) // bölmenin sonucu
+print(div) // çarpmanın sonucu
