3dc51485d32c0670ec8219fb09cf365b6152cc1c
Halil Özel <halilozel1903@gmail.com>
2018-08-16 23:52:29 +0300

    FindMyAge adlı örnek eklendi.

---
+++ FindMyAge.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// input
+
+let yearOfBirth = 1997
+
+// process
+
+let myAge = 2018 - yearOfBirth
+
+// output
+
+print(myAge)
