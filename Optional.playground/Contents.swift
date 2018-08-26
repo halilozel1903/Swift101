36f10e780056627ee8a506725e4cc5094409af3e
Halil Özel <halilozel1903@gmail.com>
2018-08-26 15:17:01 +0300

    Optional örneği eklendi.

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number : Int? // nil değer alabilir.
+
+print(number) // eğer değer atanmazsa nil değeri ekranda gösterilecek.
+
+
+let userEnteredText = "5" // sting değer
+
+let userEnteredInteger = Int(userEnteredText) // integer türe dönüştürme işlemi
+
+if let age = userEnteredInteger{ // yaş değeri aynı türde ve optional değilse
+    print(age * 8) // yaş değerini ekrana 8 ile çarpıp yazdır.
+}else{ // eğer değer optional ise
+    
+    // show an error message to the user
+    
+    print("error !!!") // hata mesajı
+}
