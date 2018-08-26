7550c37a47e6d812671d7cf239592f5aeb3c614c
Halil Özel <halilozel1903@gmail.com>
2018-08-26 15:12:39 +0300

    Optionals dersi eklendi.

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -1,108 +1,21 @@
-/*
- Swift 4, bir değerin yokluğunu işleyen Optionals türünü de tanıtıyor.
- İsteğe bağlılar ya "bir değer var, ve x eşittir" ya da "hiç bir değer yok" diyor.
- 
- İsteğe bağlı, aslında Swift 4’ün yeni süper güçlü enumlarından biri olan bir türüdür.
- İki olası değeri vardır, Yok ve Bazı (T), burada T, Swift 4'te bulunan doğru veri türünün
- ilişkili bir değeridir.
- */
-
+//: Playground - noun: a place where people can play
 
 import UIKit
 
-var perhapsInt: Int? // isteğe bağlı tam sayı bildirimi
+var number : Int? // nil değer alabilir.
 
-var perhapsStr: String? // isteğe bağlı string bildirimi
+print(number) // eğer değer atanmazsa nil değeri ekranda gösterilecek.
 
-var perhapsStr2: String? = nil  // açık bir şekilde sıfır değerine sıfırlamaya eşdeğerdir
 
-var myString:String? = nil
+let userEnteredText = "5" // sting değer
 
-if myString != nil { // eğer nil'e eşit değilse
-    print(myString) // string değerini yazdır
-} else { // nil'e eşitse
-    print("myString has nil value") // nil değerine sahip diye yazdırır.
+let userEnteredInteger = Int(userEnteredText) // integer türe dönüştürme işlemi
 
+if let age = userEnteredInteger{ // yaş değeri aynı türde ve optional değilse
+    print(age * 8) // yaş değerini ekrana 8 ile çarpıp yazdır.
+}else{ // eğer değer optional ise
+    
+    // show an error message to the user
+    
+    print("error !!!") // hata mesajı
 }
-
-
-/*
- Forced(Zorla) Unwrapping (Paketleme)
- 
- Bir değişkeni isteğe bağlı olarak tanımladıysanız, bu değişkenden değer elde etmek
- için onu paketlemeniz gerekir. Bu, değişkenin sonunda bir ünlem işareti koymak anlamına gelir.
- 
- */
-
-var myStrings:String? // string türünde bir optional tanımlandı.
-
-myStrings = "Hello, Swift 4!" // String ifadeye bir değer atandı.
-
-if myStrings != nil { // eğer string ifade nil değerine eşit değilse
-    print(myStrings) // içeriği yazdırılır. Optional("Hello, Swift 4!")
-} else { // eğer nil değerine eşitse
-    print("myStrings has nil value") // string değer nil değere sahiptir yazdırılır.
-}
-
-
-var myStringg:String?
-
-myStringg = "Hello, Swift 4!"
-
-if myStringg != nil {
-    print( myStringg! ) // değişkenin ham halini almak için Unwrapping uygulandı.
-    // ekran çıktısında ise optional ifadesi olmadan direkt içerik yazdırılacak.
-} else {
-    print("myStringg has nil value")
-}
-
-
-
-
-/* Automatic Unwrapping (Otomatik Paketleme)
- İsteğe bağlı değişkenleri soru işareti yerine ünlem işareti kullanarak bildirebilirsiniz.
- Bu isteğe bağlı değişkenler otomatik olarak kapanır ve atanan değer elde etmek için
- değişkenin sonunda başka bir ünlem işareti kullanmanız gerekmez. */
-
-
-
-var myStrinng:String! // automatic unwrapping kullanımı
-
-myStrinng = "Hello, Swift 4!" // içeriğe değer atandı.
-
-if myStrinng != nil { // eğer nil değilse Forced(Zorla) Unwrapping yapmadan direkt çalışacak.
-    print(myStrinng) // ekrana "Hello, Swift 4!" mesajı yazdırılacak.
-} else { // nil değere eşitse
-    print("myStrinng has nil value") // nil değerde diye mesaj gösterilecek.
-}
-
-
-
-/*
- 
- Optional Binding
- 
- İsteğe bağlı bir değer içerip içermediğini bulmak için isteğe bağlı optional kullanın
- ve eğer öyleyse, bu değeri geçici bir sabit veya değişken olarak kullanılabilir yapmak için kullanın.
- 
- If ifadesi için isteğe bağlı bir bağlayıcı aşağıdaki gibidir -
- 
- if let constantName = someOptional {
- statements
- }
- 
- */
-
-
-var myMessage:String? // optional bir değer tanımlandı.
-
-myMessage = "Hello, Swift 4!" // mesaj eklendi.
-
-if let yourString = myMessage { // eğer verilen değer string değere eşitse
-    print("Your string has - \(yourString)") // verilen mesaj gösterilir.
-} else { // myMessage içeriği boş olsaydı 
-    print("Your string does not have a value") // değer bulunamadı ifadesi gösterilir.
-}
-
-
-
