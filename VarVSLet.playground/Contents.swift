5276ee12a829a1751a3348cafed7d3f1918e7110
Halil Özel <halilozel1903@gmail.com>
2018-11-21 23:16:58 +0300

    Var Let örneği eklendi.

---
+++ VarVSLet.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// var : Sonradan değişiklik yapılabilen veri türüdür.
+
+// Not : Bir tür daha sonradan başka bir tür olarak direkt kullanılmaz.
+// Cast işlemi yapılması lazım
+
+var str = "Swift 4.2" // String - değişkenin türünü belirtmeden anlayabilir.
+
+var str2 : String = "Objective - C" // String - isteğe bağlı türüde belirtilebilir.
+
+print(str) // str değeri ekranda gösteriliyor.
+
+
+// let : Sonradan değiştirilemeyen veri türüdür. Sabittir. Değiştirilmeye zorlanırsa
+// ya hata verir ya da var tipine dönüştür denilir.
+
+let swiftDate = 2014 // çıkış tarihi
+
+print(swiftDate) // ekranda gösteriliyor.
